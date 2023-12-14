7476c28f8761f2f535e241682e3e1ed2ae76c070
julim <julimaimaifariaboni@gmail.com>
2023-12-14 17:29:35 -0300

    modify

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${({ theme }: any) => theme.GRAY_600};
   padding: 24px;
 `;
