ff0ea691692507f90cc561c3f24c4e6b3b7d6eb1
julim <julimaimaifariaboni@gmail.com>
2023-12-14 07:36:16 -0300

    initial commit

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+    COLORS: {
+        WHITE: '#fff',
+
+        GREEN_700: '#00875f',
+        GREEN_500: '#00b37e',
+
+        RED: '#f75a68',
+        RED_DARK: '#aa2834',
+
+        GRAY_700: '#121214',
+        GRAY_600: '#202024',
+        GRAY_500: '#29292e',
+        GRAY_400: '#323238',
+        GRAY_300: '#7c7c8a',
+        GRAY_200: '#c4c4cc',
+        GRAY_100: '#e1e1e6'
+    },
+
+    FONT_FAMILY: {
+        REGULAR: 'Roboto_400Regular',
+        BOLD: 'Bold_700Bold'
+    },
+
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24
+    }
+};
