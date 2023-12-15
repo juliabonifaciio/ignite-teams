ff61d84dc9a99cbb25048393cf0af1703a6ce4d4
julim <julimaimaifariaboni@gmail.com>
2023-12-15 17:14:00 -0300

    modify

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { StatusBar } from 'react-native';
+
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -12,6 +14,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { !fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
