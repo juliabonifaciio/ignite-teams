b81ffe9a48984ec89400a556e7608838a0bf850c
julim <julimaimaifariaboni@gmail.com>
2023-12-15 17:02:09 -0300

    modify

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
@@ -11,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups/> : <ActivityIndicator/> }
+      { !fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({ theme }: any) => theme.COLORS.GRAY_600};
+`;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }: any) => ({ 
+    color: theme.COLORS.GRAY_700
+}))``;
