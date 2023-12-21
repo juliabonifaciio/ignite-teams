a8bc3ece77f75759f2f30f66133bf8d857b5d69e
julim <julimaimaifariaboni@gmail.com>
2023-12-21 07:37:17 -0300

    modify

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups/> : <Loading/> }
+      { fontsLoaded ? <NewGroup/> : <Loading/> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+export function NewGroup() {
+    return(
+        <Container></Container>
+    );
+}
