c025e40a79a522a0576fbaadbd14b85e10b5dfca
julim <julimaimaifariaboni@gmail.com>
2024-01-01 23:40:54 -0300

    modify barStyle

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
-        barStyle="dark-content"
+        barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
